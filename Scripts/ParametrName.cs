64c6bae0d1189a8a0bdcf15dea6dcc99ce7b0edc
ExB0 <112218221+ExB0@users.noreply.github.com>
2025-06-27 05:26:34 +0900

    Update ParametrName.cs

--- Scripts/ParametrName.cs
+++ Scripts/ParametrName.cs
@@ -2,6 +2,6 @@ namespace ConsoleApp1;
 
 public class ParameterNameInMethods
 {
-    public void ShootPlayer(Player shootPlayer) {}
+    public void ShootPlayer(Player target) {}
     public string FindByIndex(int index) {}
 }
