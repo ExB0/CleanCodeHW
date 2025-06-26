63de3c1a18c38a5d8230e1016e38f535f5d0ac2f
ExB0 <112218221+ExB0@users.noreply.github.com>
2025-06-26 16:40:27 +0900

    Create EnableOrDisable.cs

---
+++ Scripts/EnableOrDisable.cs
@@ -0,0 +1,17 @@
+namespace ConsoleApp1;
+
+public class EnableOnOrOf
+{
+    public void Enable()
+    {
+        _enable = true;
+        _effects.StartEnableAnimation();
+    }
+
+    public void Disable()
+    {   
+        _enable = false;
+        _pool.Free(this);;;
+    }
+
+}
