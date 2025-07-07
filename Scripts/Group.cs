0ec1ba1749a51cd2d85513ece09a0e83a29f15f3
ExB0 <112218221+ExB0@users.noreply.github.com>
2025-07-07 19:47:43 +0900

    Update Group.cs

--- Scripts/Group.cs
+++ Scripts/Group.cs
@@ -6,10 +6,27 @@ namespace Scripts
     {
         public string Name { get; private set; }
         public int Age { get; private set; }
+    }
 
+    class Weapon
+    {
         public int WeaponDamage { get; private set; }
         public float WeaponCooldown { get; private set; }
+        public Weapon(int damage, int coldown)
+        {
+            if (damage <= 0)
+                throw new ArgumentOutOfRangeException(nameof(damage), "Урон не может быть отрицательным или равен 0");
 
+            if (coldown <= 0)
+                throw new ArgumentOutOfRangeException(nameof(coldown), "Перезарядка не может быть отрицательным или равен 0");
+
+            WeaponDamage = damage;
+            WeaponCooldown = coldown;
+        }
+    }
+
+    class PlayerMovement
+    {
         public float MovementSpeed { get; private set; }
         public float MovementDirectionX { get; private set; }
         public float MovementDirectionY { get; private set; }
@@ -18,6 +35,11 @@ namespace Scripts
         {
             //Do move
         }
+    }
+
+    class PlayerCombat
+    {
+        private Weapon _weapon = new Weapon(5, 10);
 
         public void Attack()
         {
